09e2205c5efdf1be45c19163f0940415f785fef6
Mikael Kalms <mikael@kalms.org>
2017-07-30 10:10:00 +0200

    Implemented Q-Learning AI

---
+++ TicTacToe.AI/QLearningAI.cs
@@ -0,0 +1,168 @@
+﻿
+using System;
+using System.Collections.Generic;
+using TicTacToe.Simulation;
+
+namespace TicTacToe.AI
+{
+    public class QLearningAI
+    {
+        readonly float LearningRate;
+        readonly float DiscountFactor;
+        readonly float ExplorationEpsilon;
+
+        Dictionary<string, float> Q = new Dictionary<string, float>();
+
+        Random random;
+
+        public QLearningAI(int randomSeed, float learningRate, float discountFactor, float explorationEpsilon)
+        {
+            random = new Random(randomSeed);
+            LearningRate = learningRate;
+            DiscountFactor = discountFactor;
+            ExplorationEpsilon = explorationEpsilon;
+        }
+
+        private static float WinnerToReward(BoardState.Winner winner, BoardState.Player player)
+        {
+            switch (winner)
+            {
+                case BoardState.Winner.None:
+                    return 0.0f;
+                case BoardState.Winner.Draw:
+                    return 0.5f;
+                case BoardState.Winner.Player1:
+                    return (player == BoardState.Player.Player1) ? 1.0f : -1.0f;
+                case BoardState.Winner.Player2:
+                    return (player == BoardState.Player.Player2) ? 1.0f : -1.0f;
+                default:
+                    throw new NotImplementedException();
+            }
+        }
+
+        private static string BoardStateAndActionToQID(BoardState boardState, PlayerInput action)
+        {
+            Dictionary<BoardState.Player, char> boardStateToCharacter = new Dictionary<BoardState.Player, char>
+            {
+                { BoardState.Player.None, ' ' },
+                { BoardState.Player.Player1, '1' },
+                { BoardState.Player.Player2, '2' },
+            };
+
+            string s = "";
+            for (int y = 0; y < boardState.Positions.GetLength(1); y++)
+                for (int x = 0; x < boardState.Positions.GetLength(0); x++)
+                    s += boardStateToCharacter[boardState.Positions[x, y]];
+
+            s += action.X.ToString();
+            s += action.Y.ToString();
+            s += boardStateToCharacter[action.Player];
+
+            return s;
+        }
+
+        private float ReadQ(BoardState boardState, PlayerInput action)
+        {
+            string qid = BoardStateAndActionToQID(boardState, action);
+
+            if (!Q.ContainsKey(qid))
+                return 0.0f;
+            else
+                return Q[qid];
+        }
+
+        private void WriteQ(BoardState boardState, PlayerInput action, float q)
+        {
+            string qid = BoardStateAndActionToQID(boardState, action);
+            Q[qid] = q;
+        }
+
+        private float FindMaxQ(BoardState boardState, PlayerInput[] actions)
+        {
+            if (actions == null ||actions.Length == 0)
+                throw new ArgumentException("Must supply at least one action");
+
+            float q = ReadQ(boardState, actions[0]);
+
+            for (int i = 1; i < actions.Length; i++)
+                q = Math.Max(q, ReadQ(boardState, actions[i]));
+
+            return q;
+        }
+
+        public void UpdateQ(BoardState oldBoardState, PlayerInput oldAction, BoardState newBoardState, PlayerInput[] newActions, BoardState.Winner newBoardStateWinner)
+        {
+            float reward = WinnerToReward(newBoardStateWinner, oldAction.Player);
+
+            float oldQ = ReadQ(oldBoardState, oldAction);
+
+            float maxNewQ;
+            if (newBoardStateWinner != BoardState.Winner.None)
+                maxNewQ = WinnerToReward(newBoardStateWinner, oldAction.Player);
+            else
+                maxNewQ = FindMaxQ(newBoardState, newActions);
+
+            float updatedOldQ = oldQ + LearningRate * (reward + DiscountFactor * maxNewQ - oldQ);
+
+            WriteQ(oldBoardState, oldAction, updatedOldQ);
+        }
+
+        public void UpdateQ(BoardState oldBoardState, PlayerInput oldAction, BoardState newBoardState)
+        {
+            BoardState.Winner winner = BoardState.CheckForWinner(newBoardState);
+
+            PlayerInput[] availableNewActions;
+
+            if (winner != BoardState.Winner.None)
+                availableNewActions = null;
+            else
+                availableNewActions = FindAvailableActions(newBoardState, oldAction.Player).ToArray();
+
+            UpdateQ(oldBoardState, oldAction, newBoardState, availableNewActions, winner);
+        }
+
+        private PlayerInput FindActionWithMaxQ(BoardState boardState, PlayerInput[] actions)
+        {
+            if (actions == null || actions.Length == 0)
+                throw new ArgumentException("Must supply at least one action");
+
+            PlayerInput bestAction = null;
+            float bestQ = 0.0f;
+
+            for (int i = 0; i < actions.Length; i++)
+            {
+                PlayerInput action = actions[i];
+                float q = ReadQ(boardState, action);
+                if (bestAction == null || q > bestQ)
+                {
+                    bestAction = action;
+                    bestQ = q;
+                }
+            }
+
+            return bestAction;
+        }
+
+        private static List<PlayerInput> FindAvailableActions(BoardState boardState, BoardState.Player player)
+        {
+            List<PlayerInput> availableActions = new List<PlayerInput>();
+
+            for (int y = 0; y < boardState.Positions.GetLength(1); y++)
+                for (int x = 0; x < boardState.Positions.GetLength(0); x++)
+                    if (boardState.Positions[x, y] == BoardState.Player.None)
+                        availableActions.Add(new PlayerInput(player, x, y));
+
+            return availableActions;
+        }
+
+        public PlayerInput GetPlayerInput(GameState gameState, bool explorationActive)
+        {
+            PlayerInput[] availableActions = FindAvailableActions(gameState.BoardState, gameState.NextPlayer).ToArray();
+
+            if (!explorationActive || (random.NextDouble() < ExplorationEpsilon))
+                return FindActionWithMaxQ(gameState.BoardState, availableActions);
+            else
+                return availableActions[random.Next(availableActions.Length)];
+        }
+    }
+}
--- TicTacToe.PlayAIs/Program.cs
+++ TicTacToe.PlayAIs/Program.cs
@@ -9,28 +9,62 @@ namespace TicTacToe.PlayAIs
     {
         static void Main(string[] args)
         {
-            GameState initialGameState = Simulate.CreateNewGameState();
-
             int randomSeed = (int)((DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1))).TotalSeconds);
-            RandomMoveAI ai1 = new RandomMoveAI(randomSeed + 0);
+            QLearningAI ai1 = new QLearningAI(randomSeed + 0, 0.3f, 0.9f, 0.9f);
             RandomMoveAI ai2 = new RandomMoveAI(randomSeed + 1);
 
-            const int numGames = 100000;
+            const int numTrainGames = 100000;
+            const int numEvaluateGames = 10000;
 
-            Simulate.WinnerStats winnerStats = Simulate.RunGames(initialGameState, (gameState) => {
+            TrainGames(ai1, ai2, numTrainGames);
+
+            Simulate.WinnerStats winnerStats = Simulate.RunGames(Simulate.CreateNewGameState(), (gameState) => {
                 switch (gameState.NextPlayer)
                 {
                     case BoardState.Player.Player1:
-                        return ai1.GetPlayerInput(gameState);
+                        return ai1.GetPlayerInput(gameState, false);
                     case BoardState.Player.Player2:
                         return ai2.GetPlayerInput(gameState);
                     default:
                         throw new Exception("Invalid NextPlayer");
                 }
-            }, numGames);
+            }, numEvaluateGames);
 
             Display.PrintWinnerStats(winnerStats);
 
+        }
+
+        private static void TrainGames(QLearningAI ai1, RandomMoveAI ai2, int numGames)
+        {
+            GameState initialGameState = Simulate.CreateNewGameState();
+            for (int game = 0; game < numGames; game++)
+            {
+                GameState gameState0 = initialGameState;
+                while (true)
+                {
+                    PlayerInput playerInput0 = ai1.GetPlayerInput(gameState0, true);
+                    GameState gameState1 = Simulate.Tick(gameState0, playerInput0);
+
+                    if (gameState1.Winner != BoardState.Winner.None)
+                    {
+                        ai1.UpdateQ(gameState0.BoardState, playerInput0, gameState1.BoardState);
+                        break;
+                    }
+
+                    PlayerInput playerInput1 = ai2.GetPlayerInput(gameState1);
+                    GameState gameState2 = Simulate.Tick(gameState1, playerInput1);
+
+                    if (gameState2.Winner != BoardState.Winner.None)
+                    {
+                        ai1.UpdateQ(gameState1.BoardState, playerInput1, gameState2.BoardState);
+                        break;
+                    }
+
+                    ai1.UpdateQ(gameState0.BoardState, playerInput0, gameState2.BoardState);
+
+                    gameState0 = gameState2;
+                }
+            }
         }
         /*
         static void Main(string[] args)
